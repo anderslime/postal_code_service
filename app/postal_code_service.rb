eafe26d9e98f993e011a2cb5d26d0f429c2135ea
Niels Buus <nb@lokalebasen.dk>
2013-07-08 11:43:41 +0200

    Refactored class methods to instance methods

--- app/postal_code_service.rb
+++ app/postal_code_service.rb
@@ -3,18 +3,22 @@ require './app/models/postal_code'
 
 class PostalCodeService < Sinatra::Base
 
+  configure do
+    set :postal_coder, PostalCode.new
+  end
+
   before do
     headers 'Access-Control-Allow-Origin' => '*'
     content_type :json
   end
 
   get '/' do
-    PostalCode.available_countries.to_json
+    settings.postal_coder.available_countries.to_json
   end
 
   get '/:country_code' do
     begin
-      PostalCode.find_all_by_country_code(params['country_code']).to_json
+      settings.postal_coder.find_all_by_country_code(params['country_code']).to_json
     rescue PostalCode::UnknownCountry
       status 404
     end
@@ -23,7 +27,7 @@ class PostalCodeService < Sinatra::Base
   get '/:country_code/:postal_code' do
     begin
       content_type :json
-      PostalCode.find_by_postal_code(params['country_code'], params['postal_code']).to_json
+      settings.postal_coder.find_by_postal_code(params['country_code'], params['postal_code']).to_json
     rescue PostalCode::UnknownCountry, PostalCode::UnknownCode
       status 404
     end
