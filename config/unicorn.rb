f0a93326a5a17a73a395302b1169eba588a0b94d
Jacob Atzen <jacob@incremental.dk>
2013-07-22 16:45:45 +0200

    Add unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,6 @@
 worker_processes 1 # this should be >= nr_cpus
 listen ENV['PORT']
 
-# pid "/path/to/app/shared/pids/unicorn.pid"
-# # stderr_path "/path/to/app/shared/log/unicorn.log"
-# # stdout_path "/path/to/app/shared/log/unicorn.log"
-# stderr_path "unicorn.stderr.log"
-# stdout_path "unicorn.stdout.log"
+pid         "/var/www/postal_code_service/shared/pids/unicorn.pid"
+stderr_path "/var/www/postal_code_service/shared/log/unicorn.log"
+stdout_path "/var/www/postal_code_service/shared/log/unicorn.log"
